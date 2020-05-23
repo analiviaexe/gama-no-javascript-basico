2bdce52565f5ab78eb5282c665974039c958eb47
Ana Livia <analivia.exe@gmail.com>
2020-05-23 16:36:51 -0300

    adding functions

---
+++ script.js
@@ -0,0 +1,54 @@
+function validaCpf(cpf) {
+    if(cpf.length != 11) {
+        return false;
+    } else {
+        var numeros = cpf.substring(0,9);
+        var digitos = cpf.substring(9);
+        var soma = 0;
+
+        // primeiro dígito
+        for(var i = 10; i > 1; i--){
+            soma += numeros.charAt(10 - i) * i;
+        }
+
+        console.log(soma)
+
+        var resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
+
+        if(resultado != digitos.charAt(0)){
+            return false;
+        }
+        
+        soma = 0;
+        numeros = cpf.substring(0, 10);
+
+        // segundo dígito
+        for(var j = 11; j > 1; j-- ){
+            soma += numeros.charAt(11 - j) * j;
+        }
+
+        resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
+
+        if(resultado != digitos.charAt(1)){
+            return false;
+        }
+        return true;
+    }   
+}
+
+function validacao() {
+    document.getElementById('success').style.display = 'none';
+    document.getElementById('error').style.display = 'none'
+
+    var cpf = document.getElementById("cpf_digitado").value;
+    var resultadoValidacao = validaCpf(cpf);
+
+    if(resultadoValidacao){
+        document.getElementById('success').style.display = 'block';
+    } else {
+        document.getElementById('error').style.display = 'block';
+    }
+}
+
+
+
